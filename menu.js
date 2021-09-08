1a3fbc5c0e8a6eedc7ee0ee9a6a5fee1dfdb7e50
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-09-08 23:16:04 +0100

    Use JavaScript to make the mobile menu appear and close when clicked

---
+++ menu.js
@@ -0,0 +1,17 @@
+const hamburgerButton = document.querySelector('#hamburger');
+function displayMobileMenu() {
+  const mobileMenuDiv = document.querySelector('.menu-hidden');
+   mobileMenuDiv.classList.add('menu-content');
+   hamburgerButton.style.display = 'none';
+}
+
+hamburgerButton.addEventListener('click', displayMobileMenu);
+
+function closeMobileMenu() {
+    const mobileMenuDiv = document.querySelector('.menu-hidden');
+    mobileMenuDiv.classList.remove('menu-content');
+    hamburgerButton.style.display = 'block';
+}
+
+const closeButton = document.querySelector('#close-icon');
+closeButton.addEventListener('click', closeMobileMenu);
